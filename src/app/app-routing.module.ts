2f589313fe9aa6ada15769112e34715d8e2e9917
thetechbro <fit5120groupb17@gmail.com>
2020-05-12 09:56:09 +0530

    removed about component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent }
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
