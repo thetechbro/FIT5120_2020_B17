e36ea09097986175b14d8e74e8484107fcb8a8a3
thetechbro <fit5120groupb17@gmail.com>
2020-05-12 17:05:14 +0530

    database updaated

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -18,7 +18,7 @@ const runtimeOpts = {
  */
 export const uploadData = functions.runWith(runtimeOpts).https.onRequest((request, res) => {
   let dataArr: any[] = [];
-  fs.readFile('.\\src\\scripts\\26-HousingUncertainty.csv', async (err: any, data: any) => {
+  fs.readFile('.\\src\\scripts\\Aged_care_info.csv', async (err: any, data: any) => {
     if (err) {
       console.error(err)
       return res.status(500).send({ 'data': dataArr, 'isError': true });
@@ -43,7 +43,7 @@ export const uploadData = functions.runWith(runtimeOpts).https.onRequest((reques
 export const uploadRoomData = functions.runWith(runtimeOpts).https.onRequest((request, res) => {
   let dataArr: any[] = [];
 
-  fs.readFile('.\\src\\scripts\\26-RoomDetail.csv', async (err: any, data: any) => {
+  fs.readFile('.\\src\\scripts\\Room_info.csv', async (err: any, data: any) => {
     if (err) {
       console.error(err)
       return res.status(500).send({ 'data': dataArr, 'isError': true });
@@ -60,6 +60,65 @@ export const uploadRoomData = functions.runWith(runtimeOpts).https.onRequest((re
             roomDetail: admin.firestore.FieldValue.arrayUnion(row)
           }, { merge: true });
         }
+      }
+      return res.status(200).send({ isError: false });
+    } catch (error) {
+      console.log(`Something went wrong while reading csv ${error}`)
+      return res.status(500).send({ 'data': dataArr, 'isError': true });
+    }
+  });
+});
+
+
+/**
+ * uploadFacility is a HTTP function which will upload Facilities_info.csv data to google firestore
+ */
+export const uploadFacility = functions.runWith(runtimeOpts).https.onRequest((request, res) => {
+  let dataArr: any[] = [];
+  fs.readFile('.\\src\\scripts\\Facilities_info.csv', async (err: any, data: any) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send({ 'data': dataArr, 'isError': true });
+    }
+    try {
+      dataArr = await neatCsv(data);
+      // console.log(dataArr);
+      for (const row of dataArr) {
+        console.log(row['Facility_ID'], row);
+        const docData = await db.collection('goldenstick_facility').doc(row['Facility_ID']).get();
+        if (!docData.exists) {
+          await db.collection('goldenstick_facility').doc(row['Facility_ID']).set(row);
+        } else {
+          console.log('already exist')
+        }
+      }
+      return res.status(200).send({ isError: false });
+    } catch (error) {
+      console.log(`Something went wrong while reading csv ${error}`)
+      return res.status(500).send({ 'data': dataArr, 'isError': true });
+    }
+  });
+});
+
+
+/**
+ * uploadPostcodeData is a HTTP function which will upload Postcode_CloestPostcode.csv data to google firestore
+ */
+export const uploadPostcodeData = functions.runWith(runtimeOpts).https.onRequest((request, res) => {
+  let dataArr: any[] = [];
+  fs.readFile('.\\src\\scripts\\Postcode_CloestPostcode.csv', async (err: any, data: any) => {
+    if (err) {
+      console.error(err)
+      return res.status(500).send({ 'data': dataArr, 'isError': true });
+    }
+    try {
+      dataArr = await neatCsv(data);
+      // console.log(dataArr);
+      for (const row of dataArr) {
+        const docData = await db.collection('goldenstick_data').where('STREET_PCODE', '==', row.Postcode).get();
+        docData.forEach(async (doc: any) => {
+          await db.collection('goldenstick_data').doc(doc.id).set({ 'Cloest_Postcode': row['Cloest_Postcode'] }, { merge: true })
+        });
       }
       return res.status(200).send({ isError: false });
     } catch (error) {
