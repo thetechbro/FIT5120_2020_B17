d7abf0c527a1617b43dd33303264270650502221
thetechbro <48874247+thetechbro@users.noreply.github.com>
2020-05-22 19:46:37 +1000

    added nearby facility line and corrected description spelling

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,59 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { SearchDialogComponent } from '../search-dialog/search-dialog.component';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent implements OnInit {
+  searchPincodeForm = new FormGroup({
+    zipcode: new FormControl(''),
+    organizationtype: new FormControl('')
+  });
+
+  organizationtypeList = [
+    'All',
+    'Private Incorporated Body',
+    'Religious',
+    'Community Based',
+    'State Government',
+    'Charitable'
+  ];
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  /**
+   * getHome will open search dialog with selected zipcode and type
+   * @param zipcode - zipcode
+   * @param type - organization type
+   */
+  getHome(zipcode: number, type: string) {
+    if (!zipcode && !type) {
+      this.searchPincodeForm.setErrors({
+        noinput: true
+      }, { emitEvent: true });
+      return;
+    }
+    if (zipcode && (isNaN(zipcode) || zipcode <= 0)) {
+      this.searchPincodeForm.controls.zipcode.setErrors({
+        invalid: true
+      }, { emitEvent: true });
+      return;
+    }
+    this.dialog.open(SearchDialogComponent, {
+      width: '100%',
+      height: '100%',
+      maxWidth: '100%',
+      panelClass: 'show-full-image-dialog',
+      disableClose: true,
+      autoFocus: false,
+      data: { zipcode: Number(zipcode), organizationtype: type }
+    });
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCKksmuf5XmUbWo0513J4utIE5NNB7emb8',
+    authDomain: 'goldenstick-6ec97.firebaseapp.com',
+    databaseURL: 'https://goldenstick-6ec97.firebaseio.com',
+    projectId: 'goldenstick-6ec97',
+    storageBucket: 'goldenstick-6ec97.appspot.com',
+    messagingSenderId: '766969926891',
+    appId: '1:766969926891:web:3668f42f1e16b92a2d9aae',
+    measurementId: 'G-FPDHJTK062'
+  },
+  googleMapAPI: 'AIzaSyCKksmuf5XmUbWo0513J4utIE5NNB7emb8'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
