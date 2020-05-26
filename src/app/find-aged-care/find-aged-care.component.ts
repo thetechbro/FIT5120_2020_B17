5a95f8f41fa79bdf07b8a18f82a008fcc1aebc3f
thetechbro <fit5120groupb17@gmail.com>
2020-05-26 17:18:53 +0530

    check postcode if exist or not validaation

--- src/app/find-aged-care/find-aged-care.component.ts
+++ src/app/find-aged-care/find-aged-care.component.ts
@@ -2,13 +2,14 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { SearchDialogComponent } from '../search-dialog/search-dialog.component';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 @Component({
   selector: 'app-find-aged-care',
   templateUrl: './find-aged-care.component.html',
   styleUrls: ['./find-aged-care.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 
 })
 export class FindAgedCareComponent implements OnInit {
@@ -25,7 +26,7 @@ export class FindAgedCareComponent implements OnInit {
     'State Government',
     'Charitable'
   ];
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog, private db: AngularFirestore) { }
 
   ngOnInit(): void {
   }
@@ -48,6 +49,30 @@ export class FindAgedCareComponent implements OnInit {
       }, { emitEvent: true });
       return;
     }
+    if (zipcode) {
+      const ifExistSnap = this.db.collection('goldenstick_data', ref =>
+        ref.where('STREET_PCODE', '==', zipcode.toString()).orderBy('AGED_CARE_NAME', 'asc')).valueChanges();
+      ifExistSnap.subscribe(pincode => {
+        if (pincode && pincode.length > 0) {
+          this.openSearchDialog(zipcode, type);
+        } else {
+          this.searchPincodeForm.controls.zipcode.setErrors({
+            notExist: true
+          }, { emitEvent: true });
+          return;
+        }
+      });
+    } else {
+      this.openSearchDialog(zipcode, type);
+    }
+  }
+
+  /**
+   * This method will open search dialog with aged care results.
+   * @param zipcode postcode
+   * @param type organization type
+   */
+  openSearchDialog(zipcode, type){
     this.dialog.open(SearchDialogComponent, {
       width: '100%',
       height: '100%',
